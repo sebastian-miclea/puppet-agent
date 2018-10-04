2fba272ecd492b3038b5995c1eb977dc714b7dc1
Sebastian Miclea <sebastian.miclea@puppet.com>
2018-10-04 16:59:33 +0300

    temp

--- configs/platforms/ubuntu-18.10-amd64.rb
+++ configs/platforms/ubuntu-18.10-amd64.rb
@@ -7,5 +7,8 @@ platform "ubuntu-18.04-amd64" do |plat|
   plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/debian/pl-build-tools-release-#{plat.get_codename}.deb"
   plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends build-essential devscripts make quilt pkg-config debhelper rsync fakeroot cmake"
   plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
+
+  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends libncurses5"
+
   plat.vmpooler_template "ubuntu-1804-x86_64"
 end
